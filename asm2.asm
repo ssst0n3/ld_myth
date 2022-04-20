a67b7e52f7d1f6c29b4e61315d0d95d62cf202ff
Lei Wang <ssst0n3@gmail.com>
2022-04-20 15:42:35 +0800

    update

--- asm2.asm
+++ asm2.asm
@@ -1,20 +1,14 @@
-section .text
-
           global    _start
 
-section   .data
-msg db  'Hello, world!',0xa ;our dear string
-len equ $ - msg         ;length of our dear string
-
-section   .text
-_start:   
-          mov       rdx, len                 ; number of bytes
-          mov       rsi, msg            ; address of string to output
+          section   .text
+_start:   mov       rax, 1                  ; system call for write
           mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rax, 1                  ; system call for write
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 14                 ; number of bytes
           syscall                           ; invoke operating system to do the write
-; exit via the kernel
-          xor       rdi, rdi                ; exit code 0
           mov       rax, 60                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
           syscall                           ; invoke operating system to exit
 
+          section   .rodata
+message:  db        "Hello, YHSPY!", 10      ; note the newline at the end
--- build_asm2.sh
+++ build_asm2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 nasm -f elf64 asm2.asm -o asm2.o
-ld -s -static -T asm.lds -o asm2 asm2.o
+ld -s -static -o asm2 asm2.o
+gcc -Wl,-dynamic-linker,asm2 -o hello hello.c
