f6d2d01bbc8c03f336331c85ec8680d0def373a0
Lei Wang <ssst0n3@gmail.com>
2022-04-21 14:59:28 +0800

    update

--- build_asm.sh
+++ build_asm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 filename=$1
+rm $filename.o $filename hello
 nasm -f elf64 $filename.asm -o $filename.o
-ld -s -static -T asm.lds -o $filename $filename.o
-# ld -s -static -o $filename $filename.o
+# ld -s -static -T asm.lds -o $filename $filename.o
+ld -s -static -o $filename $filename.o
 gcc -Wl,-dynamic-linker,$filename   -o hello hello.c
